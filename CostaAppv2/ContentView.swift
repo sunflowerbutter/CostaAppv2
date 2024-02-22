ffd319e4e0762d2b2add75800d151b82e13ada59
Kate Kligys <kate.kligys@gmail.com>
2024-02-21 21:09:08 -0800

    Initial Commit

---
+++ CostaAppv2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CostaAppv2
+//
+//  Created by HCD Student on 2/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CostaAppv2/CostaAppv2App.swift
@@ -0,0 +1,28 @@
+//
+//  CostaAppv2App.swift
+//  CostaAppv2
+//
+//  Created by HCD Student on 2/21/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+@main
+struct CostaAppv2App: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
